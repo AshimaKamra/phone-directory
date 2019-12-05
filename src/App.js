0b4c0891d0a07139d3ccd10b51acf2cbc6321c24
AshimaKamra <akkamra18@gmail.com>
2019-12-05 11:43:00 +0530

    External Css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
+import './App.css';
 class App extends Component {
   render() {
     return (
       
       <div>
        <Header/>
+       <div className="pageBody">
       <button>Add</button>
       <div>
       <span>Name</span><br />
       <span>Phone</span>
+      </div>
       </div>
       </div>
     );
