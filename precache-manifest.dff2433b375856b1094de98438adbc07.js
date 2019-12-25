2046c1d1aecb1eb579f9fb650aad795f2b0a0bca
AshimaKamra <akkamra18@gmail.com>
2019-12-25 12:49:48 +0530

    Updates

---
+++ precache-manifest.dff2433b375856b1094de98438adbc07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a240d1a59d0af2e0cc1f52862851dfd7",
+    "url": "/phone-directory/index.html"
+  },
+  {
+    "revision": "6e4959a73520893aeb2a",
+    "url": "/phone-directory/static/css/main.57f64ef6.chunk.css"
+  },
+  {
+    "revision": "252acaaffd3af6a5abd6",
+    "url": "/phone-directory/static/js/2.76e5bdab.chunk.js"
+  },
+  {
+    "revision": "6e4959a73520893aeb2a",
+    "url": "/phone-directory/static/js/main.a005d2d3.chunk.js"
+  },
+  {
+    "revision": "750e5236f65c3a1f5e4c",
+    "url": "/phone-directory/static/js/runtime-main.32348089.js"
+  }
+]);
